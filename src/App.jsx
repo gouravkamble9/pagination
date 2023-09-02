85724e817cf620492d02211fbdebb9a9013912f5
Gourav <gouravkamble9@gmail.com>
2023-09-02 17:08:33 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import "./App.css";
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  const [page,setPage]=useState(1)
+  const [page, setPage] = useState(1);
 
   const fetchData = async () => {
-    const res = await fetch("https://dummyjson.com/products?limit=80");
+    const res = await fetch("https://dummyjson.com/products?limit=100");
     const data = await res.json();
     setProducts(data.products);
     console.log(data.products);
@@ -17,35 +17,61 @@ const App = () => {
     fetchData();
   }, []);
 
-  const selectPageHandler=(selectedPage)=>{
-    if(selectedPage>=1 && selectedPage<=products.length / 8 && selectedPage !==page){
-      setPage(selectedPage)
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 10 &&
+      selectedPage !== page
+    ) {
+      setPage(selectedPage);
+    }
+  };
+
+  const handleNext=()=>{
+      if(page <products.length / 10 ){
+        setPage(page+1)
+      }
+  }
+
+
+  const handlePrev=()=>{
+    if(page > 1){
+      setPage(page-1);
     }
   }
 
   return (
+    <>
     <div className="products">
-      {products.slice(page * 8 - 8,page * 8).map((item, id) => (
+      {products.slice(page * 10 - 10, page * 10).map((item, id) => (
         <div key={id} className="product_card">
           <img src={item.thumbnail} alt="" />
+          <div className="images">
+            {item.images.slice(0,4).map((img,id)=>(
+              <img key={id} className={id} src={img} alt="" />
+
+            ))}
+          </div>
           <p>{item.title}</p>
         </div>
       ))}
 
-      <div className="pagination">
-        {page >1 && 
-        
-        <span onClick={()=>setPage(page - 1)}>◀️</span>
-        }
-        { [...Array(products.length / 8)].map((_,i)=>(
-          <span key={i} className={page === i+1 ? "page_selected" : ""} onClick={() => selectPageHandler(i + 1)}>{i+1}</span>
-        ))}
-        {
-          page<products.length / 8 &&
-        <span onClick={()=>setPage(page+1)}>▶️</span>
-        }
-      </div>
     </div>
+      <div className="pagination">
+      <span onClick={handlePrev}>◀️</span>
+        {[...Array(products.length / 10)].map((_, i) => (
+          <span
+            key={i}
+            className={page === i + 1 ? "page_selected" : ""}
+            onClick={() => selectPageHandler(i + 1)}
+          >
+            {i + 1}
+          </span>
+        ))}
+          <span onClick={handleNext}>▶️</span>
+      
+      </div>
+      </>
   );
 };
 
